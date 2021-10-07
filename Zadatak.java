bf1c030bd537f889773eab0f108f62cbce8a7ede
Ana Simic <anuskakv@gmail.com>
2021-10-07 19:57:22 +0200

    Anin prvi commit

---
+++ Zadatak.java
@@ -0,0 +1,20 @@
+package p19_8_2021;
+
+public class Zadatak1 {
+
+	public static void main(String[] args) {
+//		// Napisati program koji pronalazi sumu brojeva od 1 do 10 
+//		koristeci while petlju.
+
+		int i = 0;
+		int suma = 0;
+		while (i < 10) {
+			suma = suma + i;
+			i++;
+			
+			
+		}System.out.println(suma);
+
+	}
+
+}
